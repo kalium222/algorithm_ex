79ec12a5eab1167e06b23c960225fe23e8450a6a
kalium <kalium222@outlook.com>
2025-04-21 09:39:15 +0800

    implement stack with queue

---
+++ stack_and_queue/stack_with_queue.cpp
@@ -0,0 +1,36 @@
+#include <queue>
+
+class MyStack
+{
+    std::queue<int> q;
+public:
+    MyStack() {}
+    
+    void push(int x)
+    {
+        q.push(x);
+    }
+    
+    int pop()
+    {
+        int n = (int)q.size();
+        int temp;
+        for (int i=0; i<n-1; i++)
+        {
+            temp = q.front();
+            q.pop();
+            q.push(temp);
+        }
+        temp = q.front();
+        q.pop();
+        return temp;
+    }
+    
+    int top() {
+        return q.back();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
