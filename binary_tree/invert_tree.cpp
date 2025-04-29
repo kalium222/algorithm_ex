dcfa0998596d75858e4bb143abcdb4849761f192
kalium <kalium222@outlook.com>
2025-04-29 15:18:50 +0800

    invert tree

---
+++ binary_tree/invert_tree.cpp
@@ -0,0 +1,24 @@
+
+#include <utility>
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode* invertTree(TreeNode* root)
+    {
+        if (!root) return root;
+        invertTree(root->right);
+        std::swap(root->left, root->right);
+        invertTree(root->right);
+        return root;
+    }
+};
