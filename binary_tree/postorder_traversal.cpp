190d4208e2f228fbe877fe8512f8abc0721a113f
kalium <kalium222@outlook.com>
2025-05-19 11:19:53 +0800

    post order tranverse

---
+++ binary_tree/postorder_traversal.cpp
@@ -0,0 +1,96 @@
+#include <algorithm>
+#include <stack>
+#include <vector>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    vector<int> postorderTraversal(TreeNode* root)
+    {
+        return method1(root);
+    }
+
+    // recursively
+    vector<int> method1(TreeNode* root)
+    {
+        vector<int> res{};
+        traverse(root, res);
+        return res;
+    }
+
+    void traverse(TreeNode *root, vector<int> &res)
+    {
+        if (!root) return;
+        traverse(root->left, res);
+        traverse(root->right, res);
+        res.push_back(root->val);
+    }
+
+    // with stack
+    vector<int> method2(TreeNode* root)
+    {
+        stack<TreeNode*> st{};
+        vector<int> res{};
+        if (!root) return res;
+        st.push(root);
+        TreeNode *temp;
+        while (!st.empty())
+        {
+            temp = st.top();
+            st.pop();
+            res.push_back(temp->val);
+            if (temp->left) st.push(temp->left);
+            if (temp->right) st.push(temp->right);
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+
+    // with stack
+    vector<int> method3(TreeNode* root)
+    {
+        stack<TreeNode*> st{};
+        vector<int> res{};
+        TreeNode *cur = root;
+        TreeNode *prev = nullptr;
+        while (cur || !st.empty())
+        {
+            while (cur)
+            {
+                st.push(cur);
+                cur = cur->left;
+            }
+            cur = st.top();
+            st.pop();
+            if (cur->right && cur->right!=prev)
+            {
+                st.push(cur);
+                cur = cur->right;
+            }
+            else
+            {
+                res.push_back(cur->val);
+                prev = cur;
+                cur = nullptr;
+            }
+        }
+        return res;
+    }
+
+    // morris
+    vector<int> method4(TreeNode *root)
+    {
+        // TODO:
+    }
+};
