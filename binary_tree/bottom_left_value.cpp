34eaf5463bc2801ffd5a914a170dc87016c3f151
kalium <kalium222@outlook.com>
2025-05-24 12:57:26 +0800

    the bottom left value of a tree

---
+++ binary_tree/bottom_left_value.cpp
@@ -0,0 +1,36 @@
+#include <queue>
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    int findBottomLeftValue(TreeNode* root)
+    {
+        std::queue<TreeNode*> q{};
+        TreeNode *temp;
+        int s;
+        int res;
+        q.push(root);
+        while (!q.empty())
+        {
+            s = q.size();
+            res = q.front()->val;
+            for (int i=0; i<s; i++)
+            {
+                temp = q.front();
+                q.pop();
+                if (temp->left) q.push(temp->left);
+                if (temp->right) q.push(temp->right);
+            }
+        }
+        return res;
+    }
+};
