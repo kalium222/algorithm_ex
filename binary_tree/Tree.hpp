00daf2c8c2c826ebe2fd08f0923ccffa0b88ac9a
kalium <kalium222@outlook.com>
2025-06-06 12:18:54 +0800

    move the definition of Tree to a header file

---
+++ binary_tree/Tree.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+struct Tree
+{
+    int val;
+    Tree *left;
+    Tree *right;
+    Tree() : val(0), left(nullptr), right(nullptr) {}
+    Tree(int x) : val(x), left(nullptr), right(nullptr) {}
+    Tree(int x, Tree *left, Tree *right) : val(x), left(left), right(right) {}
+};
+
+typedef Tree TreeNode;
---
+++ binary_tree/construct_tree.cpp
@@ -0,0 +1,12 @@
+#include "Tree.hpp"
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder)
+    {
+
+    }
+};
