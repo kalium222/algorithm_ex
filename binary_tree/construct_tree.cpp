bd56bbc651dc30f7140dcf752976b3b8f6f66441
kalium <kalium222@outlook.com>
2025-06-06 20:53:49 +0800

    temp

--- binary_tree/construct_tree.cpp
+++ binary_tree/construct_tree.cpp
@@ -1,4 +1,5 @@
 #include "Tree.hpp"
+#include <algorithm>
 #include <vector>
 using namespace std;
 
@@ -7,6 +8,33 @@ class Solution
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder)
     {
+        if (inorder.size()==0) return nullptr;
 
+        TreeNode *root, *left, *right;
+        int root_val = postorder.back();
+        root = new TreeNode(root_val);
+
+        auto p_in = find(inorder.begin(), inorder.end(), root_val);
+        auto left_in = vector<int>(inorder.begin(), p_in);
+        auto right_in = vector<int>(p_in+1, inorder.end());
+        auto p_post = postorder.begin() + left_in.size();
+        auto left_post = vector<int>(postorder.begin(), p_post);
+        auto right_post = vector<int>(p_post, postorder.end());
+
+        if (left_in.size()==0)
+            left = nullptr;
+        else
+            left = buildTree(left_in, left_post);
+        if (right_in.size()==0)
+        {
+            right = nullptr;
+        }
+        else
+        {
+            right = buildTree(right_in, right_post);
+        }
+        root->left = left;
+        root->right = right;
+        return root;
     }
 };
