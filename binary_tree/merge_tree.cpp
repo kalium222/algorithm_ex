3220b5b9d4bdf930f24f8b87c5f694df998704aa
kalium <kalium222@outlook.com>
2025-06-08 20:33:13 +0800

    merge two trees

---
+++ binary_tree/merge_tree.cpp
@@ -0,0 +1,20 @@
+#include "Tree.hpp"
+
+class Solution
+{
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2)
+    {
+        if (!root1 && !root2)
+            return nullptr;
+        else if (root1 && root2)
+        {
+            TreeNode *root = new TreeNode(root1->val + root2->val);
+            root->left = mergeTrees(root1->left, root2->left);
+            root->right = mergeTrees(root1->right, root2->right);
+            return root;
+        }
+        else if (root1) return root1;
+        else return root2;
+    }
+};
